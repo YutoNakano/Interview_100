baefd5dbd6d3ba80407df1553f9e2dade1ac4c8a
YutoNakano <yuto.44.bb@gmail.com>
2019-06-19 12:31:50 +0900

    add: display remote data

--- Interview100/AppDelegate.swift
+++ Interview100/AppDelegate.swift
@@ -13,10 +13,18 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    let topViewController = ViewController()
+//    let questionViewController = QuestionViewController()
+    var navigationController = UINavigationController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        let presenter = QuestionPresenter(view: questionViewController)
+//        topViewController.inject(presenter: presenter)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        navigationController = UINavigationController(rootViewController: topViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         FirebaseApp.configure()
         return true
     }
--- Interview100/ViewController.swift
+++ Interview100/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 import FirebaseFirestore
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var label: UILabel = {
+        let v = UILabel()
+        view.addSubview(v)
+        return v
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.green
         // Do any additional setup after loading the view.
         let db = Firestore.firestore()
         db.collection("Questions")
-        
-        
+        .document("number1")
+            .getDocument { (document, error ) in
+        if let err = error {
+            print(err)
+        } else {
+//            print("\(document?.documentID) => \(document!.data())")
+            let text = document?.data()
+            self.label.text = text?["title"] as? String
+        }
     }
-
-
+        
+        makeConstraints()
 }
-
+    func makeConstraints() {
+        label.snp.makeConstraints { make in
+            make.size.equalTo(300)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    
+    
+    
+}
